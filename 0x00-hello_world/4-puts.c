1a79779ff841460b870bc0759248995a530f687b
kikilov3 <okikiabagun@gmail.com>
2022-09-08 16:26:29 -0700

    Hello printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Programming is like building a multilingual
- *                puzzle followed by a new line
+ * main - Prints " Programming is like building a multilingual
+ *                puzzl, followed by a new line."
+ * 
  * Return: 0
  */
 
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle");                    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 
 }	 
