8b502a643194f335e966ce614433566459901e40
kikilov3 <okikiabagun@gmail.com>
2022-09-08 16:29:19 -0700

    Hello printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,6 @@
 /**
  * main - Prints " Programming is like building a multilingual
  *                puzzl, followed by a new line."
- * 
  * Return: 0
  */
 
@@ -12,6 +11,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
-
 }	 
