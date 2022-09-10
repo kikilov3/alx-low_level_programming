a381f9b10c8edb739c0dedaae85675fa07a2096c
kikilov3 <okikiabagun@gmail.com>
2022-09-09 20:53:01 -0700

    print comb

--- 0x00-hello_world/9-print_comb.c
+++ 0x00-hello_world/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * maain -prints all possible combinations of single-digit numbers
+ * main - prints all possible combinations of single-digit numbers
  * Return: 0
  */
 int main(void)
