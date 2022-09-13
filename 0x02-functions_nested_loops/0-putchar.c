01c3990eee8500369dd86cba213cd77dbd48ff4b
kikilov3 <okikiabagun@gmail.com>
2022-09-13 10:51:01 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+/**
+ * main - print _putchar followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
