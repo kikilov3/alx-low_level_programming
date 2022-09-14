f4770f25e4a199381fc324864433ef0dc02ad48e
kikilov3 <okikiabagun@gmail.com>
2022-09-14 02:13:48 -0700

    alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
- * Return (0)
  */
 void print_alphabet_x10(void)
 {
@@ -14,5 +13,4 @@ for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
 }
-return (0);
 }
