a9a594b0e4c6344e1113cd276336c74af591039a
kikilov3 <okikiabagun@gmail.com>
2022-09-15 06:45:59 -0700

    main compound

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,6 +3,7 @@
 /**
  * main - causes an infinite loop
  * Return: 0
+ *
  */
 
 int main(void)
@@ -13,10 +14,10 @@ int main(void)
 	
 	i = 0;
 
-        /*while (i < 10) */
-        /*{ */
+        /* while (i < 10) */
+        /* { */
 	/*       putchar(i); */
-	/*} */
+	/* } */
 	
 	printf("Infinite loop avoided! \\o/\n");
 
