ceb7eed92a0001507d973440249ee7d6f51139dd
kikilov3 <okikiabagun@gmail.com>
2022-09-15 03:37:27 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,12 +7,13 @@
  * Return: largest number
  */
 int largest_number(int a, int b, int c)
-{int largest;
-if (a > b $$ a > c)
+{
+int largest;
+if (a > b && b > c)
 {
 largest = a;
 }
-else if (b > c)
+else if (b > a && a > c)
 {
 largest = b;
 }
