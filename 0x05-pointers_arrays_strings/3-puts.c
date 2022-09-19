4b73775594fb38c81683dfd1d64cb91d237111c4
kikilov3 <okikiabagun@gmail.com>
2022-09-19 14:04:36 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,11 +2,13 @@
 /**
  * _puts - prints a string in stdout
  * @str: string to print
+ * Return: void
  */
 void _puts(char *str)
 {
-while (*str)
-_putchar(*str++);
-
+for (; *str != '\0'; str++)
+{
+_putchar(*str);
+}
 _putchar('\n');
 }
