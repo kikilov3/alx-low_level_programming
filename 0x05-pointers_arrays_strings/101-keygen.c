ef3c68ab94602c14a606233a9452c9c3d32a075c
kikilov3 <okikiabagun@gmail.com>
2022-09-19 15:29:14 -0700

    hacker

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,33 @@
  */
 int main(void)
 {
-int unlock, passwd;
-
+int i, j, k, s;
+char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char p[58];
 srand(time(NULL));
-unlock = 2772;
-while (unlock >= 127)
+while (s != 2772)
 {
-passwd = (rand() % 126) + 1;
-printf("%c", passwd);
-unlock -= passwd;
+i = k = s = 0;
+while ((2772 - 122) > s)
+{
+j = rand() % 62;
+p[i] = c[j];
+s += c[j];
+i++;
 }
-printf("%c", unlock);
+while (c[k])
+{
+if (c[k] == (2772 - s))
+{
+p[i] = c[k];
+s += c[k];
+i++;
+break;
+}
+k++;
+}
+}
+p[i] = '\0';
+printf("%s", p);
 return (0);
 }
