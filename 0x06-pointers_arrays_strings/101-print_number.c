1d116c50fffdf6f4bc1815f8a23e2f48baf195ff
kikilov3 <okikiabagun@gmail.com>
2022-09-21 15:38:01 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,7 +7,6 @@
 void print_number(int n)
 {
 	unsigned int num = n;
-	
 	if (n < 0)
 	{
 		_putchar('-');
@@ -15,6 +14,5 @@ void print_number(int n)
 	}
 	if ((num / 10) > 0)
 		print_number(num / 10);
-	
 	_putchar((num % 10) + '0');
 }
